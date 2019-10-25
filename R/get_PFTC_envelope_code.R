37aefff21e789950676d33bf6049c4cd209ef163
audhalbritter <audhalbritter.com>
2019-10-25 16:37:35 +0200

    wrap supress warning around withSeed
    not important warning:
    In set.seed(seed = seed, ...) : non-uniform 'Rounding' sampler used

--- R/get_PFTC_envelope_code.R
+++ R/get_PFTC_envelope_code.R
@@ -12,7 +12,7 @@ get_PFTC_envelope_codes <- function(seed){
   all_codes <- crossing(A = LETTERS, B = LETTERS, C = LETTERS) %>%
     mutate(code = paste0(A, B, C),
            hash = (1L:n()) %% 10000L,
-           hash = withSeed(suppressWarnings(sample(hash)), seed, sample.kind = "Rounding"),
+           hash = suppressWarnings(withSeed(sample(hash), seed, sample.kind = "Rounding")),
            hash = formatC(hash, width = 4, format = "d", flag = "0"),
            hashcode = paste0(code, hash)) %>%
     select(hashcode)
