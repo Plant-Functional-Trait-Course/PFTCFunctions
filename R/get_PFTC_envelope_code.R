9b892a6f4416a8c2c3e92bd17446fccbbf450ce8
audhalbritter <audhalbritter.com>
2019-10-25 17:51:50 +0200

    replace 1:n() with row_number

--- R/get_PFTC_envelope_code.R
+++ R/get_PFTC_envelope_code.R
@@ -4,7 +4,7 @@
 #' @return tibble with IDs
 #' @examples
 #' get_PFTC_envelope_codes(seed = 1)
-#' @importFrom dplyr mutate select %>% n
+#' @importFrom dplyr mutate select %>% row_number
 #' @importFrom tidyr crossing
 #' @export
 
@@ -12,7 +12,7 @@ get_PFTC_envelope_codes <- function(seed){
   suppressWarnings(set.seed(seed = seed, sample.kind = "Rounding"))
   all_codes <- crossing(A = LETTERS, B = LETTERS, C = LETTERS) %>%
     mutate(code = paste0(A, B, C),
-           hash = (1L:n()) %% 10000L,
+           hash = row_number() %% 10000L,
            hash = sample(hash),
            hash = formatC(hash, width = 4, format = "d", flag = "0"),
            hashcode = paste0(code, hash)) %>%
