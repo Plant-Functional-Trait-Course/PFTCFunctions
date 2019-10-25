b36a13c6ef58932f27133ae86fc711cc6f55c2db
audhalbritter <audhalbritter.com>
2019-10-25 16:12:00 +0200

    remove

--- R/create_ID_list.R
+++
@@ -1,8 +0,0 @@
-# Create list with all valid IDs per country
-creat_ID_list <- function(envelope_codes){
-  all_codes <- get_envelope_codes(seed = 1) %>%
-    mutate(Site = "Peru") %>%
-    bind_rows(get_envelope_codes(seed = 32) %>%
-                mutate(Site = "Svalbard"))
-  return(all_codes)
-}
