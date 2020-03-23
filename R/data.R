2f5f373852f004800f0479bdf3ca74b5c2813663
audhalbritter <audhalbritter.com>
2020-03-23 20:28:41 +0100

    update

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Location file
+#'
+#' A dataset containing the data type and location of the data sets
+#'
+#' @format A data frame with 32 rows and 6 variables:
+#' \describe{
+#'   \item{Country}{country name}
+#'   \item{DataType}{data type name}
+#'   \item{node}{node name}
+#'   \item{file}{file name}
+#'   \item{remote_path}{remot path name}
+#'   \item{Remark}{remark name}
+#' }
+"location"
+
+#' @examples
+#' require("data/location")
+#' location %>% select(Country, DataType)
