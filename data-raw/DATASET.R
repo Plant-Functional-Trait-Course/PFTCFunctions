63306d461042ceb7f2ea3a056feabf7726ba122d
audhalbritter <me@audhalbritter.com>
2021-05-10 12:07:18 +0200

    update SV files

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,3 +4,4 @@ library("googlesheets4")
 PFTC_data_location <- read_sheet("https://docs.google.com/spreadsheets/d/1y81Xv33BaoWV2FNx0F9XbikjKeHLGNxfmWZ2w4xmISk/edit#gid=0")
 
 usethis::use_data(PFTC_data_location)
+#usethis::use_data(PFTC_data_location, overwrite = TRUE)
