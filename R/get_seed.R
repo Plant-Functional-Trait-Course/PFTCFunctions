6fbf0cce7cf8a5501c86243cf4f68a6d077349d6
RichardJTelford <Richard.Telford@uib.no>
2024-06-28 18:42:19 +0200

    code to set prefix

--- R/get_seed.R
+++ R/get_seed.R
@@ -23,3 +23,29 @@ get_seed <- function(seed) {
   }
   seed
 }
+
+
+#' get prefix from prompt
+#'
+#' @param prefix character Default value for prefix
+#' @returns Character to use as prefix
+#' @importFrom glue glue
+#' @export
+
+
+get_prefix <- function(prefix = "") {
+  finished <- FALSE
+  while (!finished) {
+    blank <- ifelse(prefix == "", "(no prefix)", "")
+    new_prefix <- readline(glue("Prefix for leaf codes is is '{prefix}' {blank}\n Type characters to change or leave blank to accept"))
+    if (new_prefix == "") {
+      finished <- TRUE
+    } else {
+      cat("\n")
+      prefix <- new_prefix
+      finished <- FALSE
+    }
+  }
+  prefix
+}
+
